347e9d21a2f2d23ea4bf5152f2c6f03176157b7c
splanes <155541.clot@fje.edu>
2018-04-12 10:41:30 +0200

    database card class

--- app/src/main/java/revolhope/splanes/com/smartcam/model/Card.java
+++ app/src/main/java/revolhope/splanes/com/smartcam/model/Card.java
@@ -33,6 +33,72 @@ public class Card {
     private String address;
     
     // TODO: Create getters & setters. Are needed!
+
+    public int getCardId() {
+        return cardId;
+    }
+
+    public void setCardId(int cardId) {
+        this.cardId = cardId;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getWeb() {
+        return web;
+    }
+
+    public void setWeb(String web) {
+        this.web = web;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+
     // Getters and setters are ignored for brevity,
     // but they're required for Room to work.
 }
