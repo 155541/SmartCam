cd3db4889aabebc14a3c270f1e5949b0a6cd7ceb
155541 <34254684+155541@users.noreply.github.com>
2018-04-13 11:27:30 +0000

    Update AppDatabase.java
    TODO: Check for context leaks

--- app/src/main/java/revolhope/splanes/com/smartcam/database/AppDatabase.java
+++ app/src/main/java/revolhope/splanes/com/smartcam/database/AppDatabase.java
@@ -12,8 +12,23 @@ import revolhope.splanes.com.smartcam.model.Card;
 @Database(entities = {Card.class, TranslationLog.class}, version = 1)
 public abstract class AppDatabase extends RoomDatabase {
     
+    private static AppDatabase appDatabase;
+    
     public abstract CardDao cardDao();
     public abstract TranslationLogDao translationLogDao();
     //public abstract NotesDao notesDao();
+        
+    public static AppDatabase getInstance(Context context)
+    { 
+        if (null == appDatabase) 
+        { 
+            appDatabase = buildDatabaseInstance(context); 
+        } 
+        return appDatabase;
+    }
     
+    private static AppDatabase buildDatabaseInstance(Context context)
+    { 
+        return Room.databaseBuilder(context, AppDatabase.class, Constants.DB_NAME).build();
+    }
 }
