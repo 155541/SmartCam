fc02d51dc123dd9bd789c74ed2ae5a886e245321
155541 <34254684+155541@users.noreply.github.com>
2018-04-13 09:54:20 +0000

    Create PENDING.java

---
+++ app/src/main/java/revolhope/splanes/com/smartcam/PENDING.java
@@ -0,0 +1,21 @@
+
+
+Create the next package tree: 
+
+  helper:
+    ocr:
+      OcrGraphic
+      OcrDetectorProcessor.java
+    ui_camera:
+      CameraSource.java
+      GraphicOverlay.java
+      CameraSourcePreview.java
+    translator:
+      TranslatorProcessor.java
+      
+     
+     TranslatorProcessor.java -->
+     
+     public class TranslatorProcessor {
+     }
+    
