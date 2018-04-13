e90e8c0949b58ef6550816c6677ce0945c4b9442
155541 <34254684+155541@users.noreply.github.com>
2018-04-13 11:27:27 +0000

    Update Constants.java

--- app/src/main/java/revolhope/splanes/com/smartcam/Constants.java
+++ app/src/main/java/revolhope/splanes/com/smartcam/Constants.java
@@ -1,5 +1,9 @@
 public class Constants
 {  
+  
+  // Database constants
+  public static final String DB_NAME = "AppDatabase";
+  
   // Translation constants
   public static final String TRANSLATION_MODEL_NMT = "nmt";
   public static final String TRANSLATION_MODEL_BASE = "base";
