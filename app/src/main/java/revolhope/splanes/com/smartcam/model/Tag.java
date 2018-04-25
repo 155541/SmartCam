e61ac547833d1e3982c924f232575d7bc4a96cd8
155541 <34254684+155541@users.noreply.github.com>
2018-04-25 10:56:12 +0200

    Update Tag.java

--- app/src/main/java/revolhope/splanes/com/smartcam/model/Tag.java
+++ app/src/main/java/revolhope/splanes/com/smartcam/model/Tag.java
@@ -1,27 +1,38 @@
 package revolhope.splanes.com.smartcam.model;
 
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+
 @Entity(tableName = "table_tag", 
         indices = {@Index(value = {"tag_name"}, unique = true)})
 public class Tag {
     
     @PrimaryKey
     @ColumnInfo(name = "tag_id")
-    private int tagId;
+    private String tagId;
     
     @ColumnInfo(name = "tag_name")
     private String tagName;
 
-    public Tag(int tagId, String tagName)
+    @Ignore
+    public Tag(String tagName)
+    {
+        this.tagId = UUID.randomUUID().toString();
+        this.tagName = tagName;
+    }
+        
+    public Tag(String tagId, String tagName)
     {
         this.tagId = tagId;
         this.tagName = tagName;
     }
 
-    public int getTagId() {
+    public String getTagId() {
         return tagId;
     }
 
-    public void setTagId(int tagId) {
+    public void setTagId(String tagId) {
         this.tagId = tagId;
     }
 
